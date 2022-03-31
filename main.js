66f86714fac44d01506b716d327d049d7d0b05ff
clio001 <johnwoitkowitz@hotmail.com>
2022-03-31 17:20:13 +0200

    h1 and div container added

--- main.js
+++ main.js
@@ -1,3 +1,47 @@
-function MyFirstFunction() {
-    alert("Hello");
+// JAVASCRIPT EXERCISES
+//
+// 1. Variables and Expressions
+//
+// Exercise 1
+
+let myName = "John"
+console.log("My name is " + myName)
+
+
+// Excercise 2
+
+let myAge = 39
+console.log("I am " + myAge + " years old")
+
+
+// Exercise 3
+
+let juliaAge = 32
+let ageDiff = myAge - juliaAge
+console.log("John is " + ageDiff + " years older than Julia")
+
+// 2. Conditional statements
+//
+// Exercise 4
+
+if (myAge > 21) {
+    console.log("John is older than 21.")
+} else {
+    console.log("John is younger than 21.")
 }
+
+
+// Exercise 5
+
+if (myAge > juliaAge) {
+    console.log("John is older than Julia.")
+} else if (myAge == juliaAge) {
+    console.log("John and Julia share the same age.")
+} else {
+    console.log("Julia is older than John.")
+}
+
+// Sorting an Array
+//
+// Exercise 1
+
